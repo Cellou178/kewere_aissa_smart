7cd17e34ee56e5a21e541eecf85529321e40af3e
Cellou Diallo <celloudiallo286@gmail.com>
2026-06-01 16:43:49 +0000

    feat: super admin dashboard

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4074387067" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2354062758" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
