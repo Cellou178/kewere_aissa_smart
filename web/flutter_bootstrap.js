6f2cd8195c854232de9bba01068e409dc8124476
Cellou Diallo <celloudiallo286@gmail.com>
2026-06-02 10:51:09 +0000

    feat: ecran suspension/resiliation entreprise dynamique

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3751748775" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4206468490" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
