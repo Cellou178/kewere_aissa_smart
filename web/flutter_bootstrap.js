c7d80fe001f31ddc293756ba358f850dce14f4aa
Cellou Diallo <celloudiallo286@gmail.com>
2026-06-02 22:48:44 +0000

    fix: corriger encodage tous les ecrans - accents et emojis corrompus

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "918288012" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "277096144" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
